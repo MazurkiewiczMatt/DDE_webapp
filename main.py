c4d590a11c205464f3015c450cc397ba95c8ef48
MazurkiewiczMatt <mateusz.a.mazurkiewicz@gmail.com>
2024-10-15 21:35:57 +0200

    More options

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ st.image("diagram_time_delay.png", caption="The circuit modelled using temporal 
 st.markdown("Cavities:")
 with st.container(border=True):
 
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.columns([1, 2, 2])
 
     with col1:
-        st.markdown("Settings:")
         cavities = st.radio(
             " ",
             ["$\omega_1 = \omega_2$", "$\omega_1 \\neq \omega_2$"],
@@ -22,17 +21,21 @@ with st.container(border=True):
     with col2:
         with st.container(border=True):
             st.markdown("Cavity A")
-            omega_1 = st.number_input("$\omega_1$ [Hz]", value=1.0)
-            kappa_w1 = st.number_input("$\kappa_{w,1}/\omega_1$", value=1.0, help="$\kappa_{w,1}$ is coupling with waveguide.")
-            kappa_x1 = st.number_input("$\kappa_{x,1}/\omega_1$", value=1.0, help="$\kappa_{x,1}$ represents other cavity losses.")
+            omega_1 = st.number_input("$\omega_1$ [Hz]", value=1.0, help="Natural frequency of the cavity.")
+            kappa_w1_ratio = st.number_input("$\kappa_{w,1}/\omega_1$", value=0.1, help="$\kappa_{w}$ is coupling with waveguide.")
+            kappa_x1_ratio = st.number_input("$\kappa_{x,1}/\omega_1$", value=0.0, help="$\kappa_{x}$ represents other cavity losses.")
 
     with col3:
         with st.container(border=True):
             st.markdown("Cavity B")
             if cavities == "$\omega_1 \\neq \omega_2$":
-                omega_2 = st.number_input("$\omega_2$ [Hz]", value=1.0)
+                omega_2 = st.number_input("$\omega_2$ [Hz]", value=1.0, help="Natural frequency of the cavity.")
             else:
                 omega_2 = st.number_input("$\omega_2$ [Hz]", value=omega_1, disabled=True)
+            kappa_w2_ratio = st.number_input("$\kappa_{w,2}/\omega_2$", value=0.1,
+                                       help="$\kappa_{w}$ is coupling with waveguide.")
+            kappa_x2_ratio = st.number_input("$\kappa_{x,2}/\omega_2$", value=0.0,
+                                       help="$\kappa_{x}$ represents other cavity losses.")
 
 
 
