ab9108de0b94506f72884f130a47f4cd3db9090f
MazurkiewiczMatt <mateusz.a.mazurkiewicz@gmail.com>
2024-10-15 14:17:13 +0200

    Formatting title

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.markdown("# Recirculating Photonic Integrated Circuits for Machine Learning \n ## MSc Thesis, Mateusz Mazurkiewicz, 15-10-2024")
+st.markdown("## Recirculating Photonic Integrated Circuits for Machine Learning \n ### MSc Thesis, Mateusz Mazurkiewicz, 15-10-2024")
 
 with st.container(border=True):
     st.markdown("Cavities:")
